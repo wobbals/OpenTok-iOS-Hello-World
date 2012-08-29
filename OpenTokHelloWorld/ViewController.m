620bf5ab683bd3a69bfff187cb345d9c9d01494c
Charley Robinson <charley@tokbox.com>
2012-08-29 11:21:01 -0700

    workaround for friendly use with production environment without devtoken

--- OpenTokHelloWorld/ViewController.m
+++ OpenTokHelloWorld/ViewController.m
@@ -15,12 +15,14 @@
 }
 static double widgetHeight = 240;
 static double widgetWidth = 320;
-static NSString* const kApiKey = @"1127";
-static NSString* const kToken = @"devtoken";
-static NSString* const kSessionId = @"1sdemo00855f8290f8efa648d9347d718f7e06fd";
-                                // To test the session in a web page,
-                                // go to http://staging.tokbox.com/opentok/api/tools/js/tutorials/helloworld.html
-                                // For a unique API key, go to http://staging.tokbox.com/hl/session/create
+
+#import "token.h"
+// You will need to make your own session IDs and tokens in your app.
+// For a unique API key, go to http://www.tokbox.com/opentok/api/documentation/gettingstarted#getAPIKey
+static NSString* const kApiKey = @"11421872";
+static NSString* const kToken = kOpenTokHelloWorldToken;
+static NSString* const kSessionId = @"2_MX4xMTQyMTg3Mn43Mi41LjE2Ny4xMzR-VHVlIEF1ZyAyOCAxNDo1Njo0MiBQRFQgMjAxMn4wLjk3MjIxMTV-";
+
 static bool subscribeToSelf = YES; // Change to NO if you want to subscribe to streams other than your own.
 
 #pragma mark - View lifecycle
